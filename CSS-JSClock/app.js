291f6baea95494d4e6a73eeae1d89671dc22ebb4
Shav <shavenchew@yahoo.com>
2016-12-10 21:28:16 -0800

    Mostly working. Hour hand doesn't move until a full hour has passed

--- CSS-JSClock/app.js
+++ CSS-JSClock/app.js
@@ -6,8 +6,22 @@ var hourHand   = document.querySelector('.hour-hand');
 function moveClock(){
   var now = new Date();
   var seconds = now.getSeconds();
+
   var secondsDegrees = ((seconds/60) * 360) + 90;
   secondHand.style.transform = 'rotate('+secondsDegrees+'deg)';
+
+  var minutes = now.getMinutes();
+  var minuteDegrees = ((minutes/60) * 360) + 90;
+  minuteHand.style.transform = 'rotate('+minuteDegrees+'deg)';
+
+
+  var hours = now.getHours();
+  if(hours > 12){
+    hours -= 12;
+  }
+  hourDegrees = ((hours/12) * 360) + 90;
+  console.log(hourDegrees);
+  hourHand.style.transform = 'rotate('+hourDegrees+'deg)';
 }
 
 setInterval(moveClock, 1000);
